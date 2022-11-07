132420d7222bc83b18de10cdfd8040f77f849e3f
Nino Faust <nfaust@z1r8p4.42lyon.fr>
2022-11-07 21:29:54 +0100

    folder libft added

---
+++ Makefile
@@ -0,0 +1,56 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/11/07 20:54:45 by nfaust            #+#    #+#              #
+#    Updated: 2022/11/07 21:13:42 by nfaust           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = libft.a
+CC = gcc
+FLAGS = -Wall -Werror -Wextra
+HEADER = libft.h
+REMOVE = rm -f
+ARCHIVE = ar rc
+SOURCES = ft_atoi.c \
+	ft_calloc.c \
+	ft_isalpha.c \
+	ft_isdigit.c \
+	ft_memchr.c \
+	ft_memcpy.c \
+	ft_memset.c \
+	ft_strdup.c \
+	ft_strlcpy.c \
+	ft_strncmp.c \
+	ft_strrchr.c \
+	ft_toupper.c \
+	ft_bzero.c \
+	ft_isalnum.c \
+	ft_isascii.c \
+	ft_isprint.c \
+	ft_memcmp.c \
+	ft_memmove.c \
+	ft_strchr.c \
+	ft_strlcat.c \
+	ft_strlen.c \
+	ft_strnstr.c \
+	ft_tolower.c
+
+OBJ = $(SOURCE: .c=.o)
+
+${NAME}:    ${OBJ}
+            ${AR} ${NAME} ${OBJ}
+
+all:        ${NAME}
+
+clean:
+            ${RM} ${OBJ}
+
+fclean:
+            ${RM} ${OBJ} ${NAME}
+
+re:    fclean ${NAME}
---
+++ libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 17:14:40 by nfaust            #+#    #+#             */
+/*   Updated: 2022/11/07 18:01:28 by nfaust           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <string.h>
+# include <strings.h>
+
+int		ft_isalpha(int i);
+int		ft_isdigit(int i);
+int		ft_isalnum(int i);
+int		ft_isascii(int i);
+int		ft_isprint(int i);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+#endif
