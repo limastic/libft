a6b8da8c89635d0ccda869d4319507b822206104
Nino Faust <nfaust@z4r5p5.42lyon.fr>
2022-11-08 15:26:35 +0100

    updated libft.h and added memcpy, tolower and toupper

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,24 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:50:39 by nfaust            #+#    #+#             */
-/*   Updated: 2022/11/08 14:50:40 by nfaust           ###   ########.fr       */
+/*   Updated: 2022/11/08 15:15:29 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	void	*dstcpy;
+	char	*string;
+	char	*srccpy;
+	int		i;
+
+	dstcpy = dst;
+	i = 0;
+	string = (char *) dst;
+	srccpy = (char *) src;
+	while ((size_t) i < n)
+		string[i] = srccpy[i];
+	return (dstcpy);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:14:40 by nfaust            #+#    #+#             */
-/*   Updated: 2022/11/07 18:01:28 by nfaust           ###   ########.fr       */
+/*   Updated: 2022/11/08 15:02:18 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
