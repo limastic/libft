d0a270f9ab23eaab5a12bf836a3ea0012ee1c688
Nino Faust <nfaust@z1r3p5.42lyon.fr>
2022-11-11 10:23:07 +0100

    added working atoi

---
+++ ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 09:39:45 by nfaust            #+#    #+#             */
+/*   Updated: 2022/11/11 10:21:20 by nfaust           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr_is_negative;
+	int	result;
+
+	nbr_is_negative = 0;
+	result = 0;
+	i = 0;
+	while (str[i] == 32 || (9 <= str[i] && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+				nbr_is_negative = 1;
+	while (str[i])
+	{
+		if ('0' <= str[i] && str[i] <= '9')
+			result = result * 10 + str[i++] - '0';
+		else if (result)
+			break ;
+		else
+			return (0);
+	}
+	if (nbr_is_negative)
+		result = (long)result * -1;
+	return (result);
+}
