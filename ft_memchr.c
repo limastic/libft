e7a8bcba7db6b10dee79ae22258ab61cc7c6cd90
Nino Faust <nfaust@z2r7p4.42lyon.fr>
2022-11-15 02:06:29 +0100

    patched memchr and memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:09:37 by nfaust            #+#    #+#             */
-/*   Updated: 2022/11/09 17:26:20 by nfaust           ###   ########.fr       */
+/*   Updated: 2022/11/15 00:26:21 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	ch;
-	char			*str;
+	unsigned char	*str;
 	int				i;
 
-	str = (char *)s;
+	str = (unsigned char *)s;
 	ch = (unsigned char) c;
 	i = -1;
 	while ((size_t)++i < n)
 		if (ch == str[i])
-			return ((void *) s + i);
+			return ((void *) str + i);
 	return (NULL);
 }
