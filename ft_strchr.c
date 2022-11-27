34c5eb25c70869dd66a8f813f617cd2e1ec029a8
Nino Faust <nfaust@z2r3p6.42lyon.fr>
2022-11-27 05:02:48 +0100

    added getNextLine

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,21 +6,23 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:26:29 by nfaust            #+#    #+#             */
-/*   Updated: 2022/11/15 02:22:14 by nfaust           ###   ########.fr       */
+/*   Updated: 2022/11/25 07:24:39 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
 	char		ch;
 	size_t		i;
 
 	ch = (char) c;
 	i = 0;
+	if (!s)
+		return (NULL);
 	while (s[i++] != ch)
 		if (i > ft_strlen(s))
 			return (NULL);
-	return ((char *) s + i - 1);
+	return (s + i - 1);
 }
