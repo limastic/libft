13a20f33615c698772fe6f15823fe7a12f809711
Nino Faust <nfaust@z4r11p1.42lyon.fr>
2022-12-06 16:21:57 +0100

    added push_swap

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:26:29 by nfaust            #+#    #+#             */
-/*   Updated: 2022/11/25 07:24:39 by nfaust           ###   ########.fr       */
+/*   Updated: 2022/12/05 18:50:20 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	char		ch;
 	size_t		i;
@@ -24,5 +24,5 @@ char	*ft_strchr(char *s, int c)
 	while (s[i++] != ch)
 		if (i > ft_strlen(s))
 			return (NULL);
-	return (s + i - 1);
+	return ((char *)(s + i - 1));
 }
