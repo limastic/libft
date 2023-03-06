f2d149d516e6316894ba2c544f1b2c1193a0135d
Nino Faust <nfaust@z4r2p7.42lyon.fr>
2023-03-06 15:14:36 +0100

    patched croy bug

--- ft_dup_and_free.c
+++ ft_dup_and_free.c
@@ -6,7 +6,7 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:35:40 by nfaust            #+#    #+#             */
-/*   Updated: 2023/03/04 15:46:11 by nfaust           ###   ########.fr       */
+/*   Updated: 2023/03/06 15:05:15 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 char	*ft_dup_and_free(char **two_dim_array, size_t index)
 {
 	char	*save_element;
+	size_t	max_index;
 
 	if (!two_dim_array)
 		return (NULL);
+	max_index = 0;
+	while (two_dim_array[max_index])
+		max_index++;
+	if (index >= max_index)
+		return (ft_free_twodimarr(two_dim_array), NULL);
 	save_element = ft_strdup(two_dim_array[index]);
 	if (!save_element)
 		return (ft_free_twodimarr(two_dim_array), NULL);
--- ft_printfree.c
+++ ft_printfree.c
@@ -6,7 +6,7 @@
 /*   By: nfaust <nfaust@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 09:06:31 by nfaust            #+#    #+#             */
-/*   Updated: 2023/03/06 09:10:43 by nfaust           ###   ########.fr       */
+/*   Updated: 2023/03/06 14:52:16 by nfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_printfree(char *str)
 {
 	size_t	str_len;
 
-	if (!str)
-		return ;
+	if (!str || !str[0])
+		return (write(1, "\"\"\n", 3), (void) 0);
 	str_len = ft_strlen(str);
 	write(1, str, str_len);
 	write(1, "\n", 1);
